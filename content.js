d44da21a62a09f3544d37b667fdfa82e6d956a13
arima0714 <32933855+arima0714@users.noreply.github.com>
2023-10-14 15:46:33 +0900

    フォーマットと説明の追加

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
-document.body.innerHTML = document.body.innerHTML.replace(/コメント/g, '🌾🌾🌾');
-document.body.innerHTML = document.body.innerHTML.replace(/JavaScript/g, '🌾🌾🌾');
-document.body.innerHTML = document.body.innerHTML.replace(/ECMAScript/g, '🌾🌾🌾');
+if (chrome.storage.local.get(['extensionState']) != true) {
+    document.body.innerHTML = document.body.innerHTML.replace(/コメント/g, '🌾🌾🌾');
+    document.body.innerHTML = document.body.innerHTML.replace(/JavaScript/g, '🌾🌾🌾');
+    document.body.innerHTML = document.body.innerHTML.replace(/ECMAScript/g, '🌾🌾🌾');
+}
