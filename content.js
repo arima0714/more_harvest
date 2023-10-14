f2919e56c0dc1a28c3fbd55c0e491101e50dec6f
arima0714 <32933855+arima0714@users.noreply.github.com>
2023-10-14 15:07:42 +0900

    フォーマットの実施

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
-document.body.innerHTML=document.body.innerHTML.replace(/コメント/g,'🌾🌾🌾');
-document.body.innerHTML=document.body.innerHTML.replace(/JavaScript/g,'🌾🌾🌾');
-document.body.innerHTML=document.body.innerHTML.replace(/ECMAScript/g,'🌾🌾🌾');
+document.body.innerHTML = document.body.innerHTML.replace(/コメント/g, '🌾🌾🌾');
+document.body.innerHTML = document.body.innerHTML.replace(/JavaScript/g, '🌾🌾🌾');
+document.body.innerHTML = document.body.innerHTML.replace(/ECMAScript/g, '🌾🌾🌾');
