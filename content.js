bd3bde4419cacef76f1b1d534ba6d0ec27579332
arima0714 <32933855+arima0714@users.noreply.github.com>
2023-10-15 12:50:51 +0900

    アイコンの追加
    * 微修正

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
 if (chrome.storage.local.get(['extensionState']) != true) {
     document.body.innerHTML = document.body.innerHTML.replace(/コメント/g, '🌾🌾🌾');
-    document.body.innerHTML = document.body.innerHTML.replace(/JavaScript/g, '🌾🌾🌾');
-    document.body.innerHTML = document.body.innerHTML.replace(/ECMAScript/g, '🌾🌾🌾');
 }
