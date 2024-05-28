83624fd5fa8cef74a826d16182ef01728c607899
arima0714 <32933855+arima0714@users.noreply.github.com>
2024-05-28 17:54:49 +0900

    2024/05/28

--- popup.js
+++ popup.js
@@ -3,6 +3,12 @@ document.getElementById('toggleExtension').addEventListener('change', (event) =>
     chrome.storage.local.set({extensionState: event.target.checked});
 });
 
+document.getElementById('toggleExtension').addEventListener('change', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.reload(tabs[0].id);
+    });
+});
+
 // ポップアップが読み込まれたときに、保存されている状態を読み込みます。
 chrome.storage.local.get(['extensionState'], (result) => {
     document.getElementById('toggleExtension').checked = result.extensionState;
